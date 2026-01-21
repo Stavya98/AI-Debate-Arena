4f53eb2e15e783d2f327ddc484ab5f692dc15cca
Stavya Joshi <joshistavya@gmail.com>
2026-01-21 21:54:51 +0530

    fixed for deployment

--- auth.py
+++ auth.py
@@ -2,7 +2,24 @@
 import firebase_admin
 from firebase_admin import credentials, auth
 
-cred = credentials.Certificate("D:/code/debate arena/ai-debate-arena-sj-firebase-adminsdk-fbsvc-0ba3debe2e.json")
+#cred = credentials.Certificate("D:/code/debate arena/ai-debate-arena-sj-firebase-adminsdk-fbsvc-0ba3debe2e.json")
+#this line is used for deployment
+import os
+import json
+import firebase_admin
+from firebase_admin import credentials, auth
+
+if not firebase_admin._apps:
+    firebase_json = os.environ.get("FIREBASE_SERVICE_ACCOUNT")
+
+    if not firebase_json:
+        raise RuntimeError("Missing FIREBASE_SERVICE_ACCOUNT env variable")
+
+    cred = credentials.Certificate(json.loads(firebase_json))
+    firebase_admin.initialize_app(cred)
+#from here its common
+
+
 firebase_admin.initialize_app(cred)
 
 def verify_user(token:str):
